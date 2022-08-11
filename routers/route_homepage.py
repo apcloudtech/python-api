68c2efd1c1a2660374e99606abc2f9162ad9759f
ap cloudtech <apcloudtech@gmail.com>
2022-08-10 18:34:53 -0700

    edit gunicorn.service filename

--- routers/route_homepage.py
+++ routers/route_homepage.py
@@ -12,4 +12,4 @@ general_pages_router = APIRouter()
 
 @general_pages_router.get("/")
 async def home(request: Request):
-    return templates.TemplateResponse("general_pages/homepage.html", {"request": request})
+    return templates.TemplateResponse("general_pages/index.html", {"request": request})
